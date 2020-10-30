7f7ab1d2404c2ffed69868327e4d55f3efb2f997
Such <360996299@qq.com>
2020-10-30 19:40:29 +0800

    detail

--- pages/detail/Detail.vue
+++ pages/detail/Detail.vue
@@ -28,7 +28,7 @@
 	  	</u-card>
 		<!-- 商品编码 -->
 	    <u-card padding="10" box-shadow="true" border title="商品编码" sub-title="点击卡片复制编码" title-color="#6E6E6E" 
-		title-size="25" sub-title-color="#2b85e4" sub-title-size="25" :head-style="headStyle" @click="copyHscode">
+		title-size="25" sub-title-color="#2b85e4" sub-title-size="25" :head-style="headStyle" @click="copyHscode(list.hscode)">
 	    		<view class="" slot="body">
 	  			<view>
 	    			<view class="u-body-item u-flex u-border-bottom u-col-between u-p-t-0">
@@ -39,6 +39,19 @@
 	  			</view>
 	    		</view>
 	    	</u-card>
+		<!-- 商品分类 -->
+		<u-card padding="10" box-shadow="true" border title="商品分类" sub-title="点击卡片复制分类" title-color="#6E6E6E" 
+		title-size="25" sub-title-color="#2b85e4" sub-title-size="25"  :head-style="headStyle" @click="copyCate(list.hscode_name)">
+				<view class="" slot="body">
+				<view>
+					<view class="u-body-item u-flex u-border-bottom u-col-between u-p-t-0">
+						<view class="u-body-item-title u-line-2">
+						<span style="font-size: 20px;font-weight: bolder;"  class="span_block">{{list.hscode_name}}</span>
+					</view>
+					</view>
+				</view>
+				</view>
+			</u-card>
 		<!-- 计量单位 -->
 		<u-card padding="10" box-shadow="true" border title="计量单位" title-color="#6E6E6E" title-size="25" :head-style="headStyle">
 				<view class="" slot="body">
@@ -55,11 +68,13 @@
 						</view>
 						<view>
 						<span style="font-size: 13px;font-weight: bold;color: #606266;">法定第二计量单位名称</span>
-						<span style="font-size: 15px;font-weight: bolder;color: #19be6b;" class="span_right">{{list.unit_2_name}}</span>
+						<span style="font-size: 15px;font-weight: bolder;color: #19be6b;" class="span_right" v-if="list.unit_2_name !== '' ">{{list.unit_2_name}}</span>
+						<span style="font-size: 15px;font-weight: bolder;color: #909399;" class="span_right" v-else>无</span>
 						</view>
 						<view>
 						<span style="font-size: 13px;font-weight: bold;color: #606266;">法定第二计量单位</span>
-						<span style="font-size: 15px;font-weight: bolder;color: #19be6b;" class="span_right">{{list.unit_2}}</span>
+						<span style="font-size: 15px;font-weight: bolder;color: #19be6b;" class="span_right" v-if="list.unit_2 !== '' ">{{list.unit_2}}</span>
+						<span style="font-size: 15px;font-weight: bolder;color: #909399;" class="span_right" v-else>无</span>
 						</view>
 					</view>
 					</view>
@@ -102,17 +117,15 @@
 					</view>
 				</u-card>
 				<!-- 监管条件 -->
-				<u-card padding="10" box-shadow="true" border title="监管条件" title-color="#6E6E6E" title-size="25" :head-style="headStyle">
+				<u-card padding="10" box-shadow="true" border title="监管条件" :sub-title="list.regulatory_code" title-color="#6E6E6E" title-size="25" :head-style="headStyle">
 						<view class="" slot="body">
 						<view>
 							<view class="u-body-item u-flex u-border-bottom u-col-between u-p-t-0">
 								<view class="u-body-item-title u-line-2">
 								<span style="font-size: 15px;color: #909399;" v-if="list.regulatory_code === '' ">暂无</span>
-								<view v-else>
-								<span style="font-size: 20px;font-weight: bolder;">{{list.regulatory_code}}</span>
-								<view>
-								<span style="font-size: 13px;color: #606266;">{{list.regulatory_code_name}}</span>
-								</view>
+								<view v-else v-for="(code,c) in codeName" :key="c">
+								<span style="font-size: 15px;font-weight: bolder;">{{code}}</span>
+								<span style="font-size: 15px;font-weight: bold;color: #606266;" class="span_right">{{codeDetail[c]}}</span>
 								</view>
 							</view>
 							</view>
@@ -120,17 +133,15 @@
 						</view>
 					</u-card>
 					<!-- 检验检疫类别 -->
-					<u-card padding="10" box-shadow="true" border title="检验检疫类别" title-color="#6E6E6E" title-size="25" :head-style="headStyle">
+					<u-card padding="10" box-shadow="true" border title="检验检疫类别" :sub-title="list.ciq_code" title-color="#6E6E6E" title-size="25" :head-style="headStyle">
 							<view class="" slot="body">
 							<view>
 								<view class="u-body-item u-flex u-border-bottom u-col-between u-p-t-0">
 									<view class="u-body-item-title u-line-2">
 									<span style="font-size: 15px;color: #909399;" v-if="list.ciq_code === '' ">暂无</span>
-									<view v-else>
-									<span style="font-size: 20px;font-weight: bolder;">{{list.ciq_code}}</span>
-									<view>
-									<span style="font-size: 13px;color: #606266;">{{list.ciq_code_name}}</span>
-									</view>
+									<view v-else v-for="(ciq,q) in ciqName" :key="q">
+									<span style="font-size: 15px;font-weight: bolder;">{{ciq}}</span>
+									<span style="font-size: 15px;font-weight: bold;color: #606266;" class="span_right">{{ciqDetail[q]}}</span>
 									</view>
 								</view>
 								</view>
@@ -143,12 +154,14 @@
 							<view>
 								<view class="u-body-item u-flex u-border-bottom u-col-between u-p-t-0">
 									<view class="u-body-item-title u-line-2">
+										<span style="font-size: 15px;color: #909399;" v-if="list.element_require === '' ">暂无</span>
+										<view v-else>
 											<u-row>
 												<u-col span="4" v-for="(item,i) in elementList" :key="i">
 													<u-tag :text="item" type="primary"/>
 												</u-col>
 											</u-row>
-									<!-- </span> -->
+											</view>
 									</view>
 								</view>
 								</view>
@@ -166,8 +179,8 @@
   export default {
     data() {
       return {
-		  // 关键词
-		  key: '',
+		// 关键词
+		key: '',
         // 商品详情数据列表
         goodList: [],
 		//卡片头部样式
@@ -176,6 +189,7 @@
 		},
 		title:'',
 		hscode:'',
+		cate:'',
 		//申报要素列表
 		elementList:[],
 		//搜索确认按钮样式
@@ -188,7 +202,15 @@
 			'line-height':'25px'
 		},
 		//回到顶部
-		scrollTop:0
+		scrollTop:0,
+		//监管条件名称列表
+		codeName:[],
+		//监管条件详情列表
+		codeDetail:[],
+		//检疫条件名称列表
+		ciqName:[],
+		//检疫条件详情列表
+		ciqDetail:[]
       }
     },
 	onPageScroll(e) {
@@ -212,8 +234,11 @@
 					arr.push(res.data.data.info)
 					this.goodList = arr
 					this.title = this.$Route.query.title
-					this.hscode = this.$Route.query.hscode
-					this.getElementList()
+					// console.log(this.goodList)
+					this.codeList()
+					this.ciqList()
+					this.getElementList(this.goodList)
+					this.changeNumber()
 					},
 			fail:(res)=>{
 				return this.$refs.uTips.show({
@@ -244,14 +269,16 @@
 		  this.goodList.forEach(item =>{
 			return element = item.element_require
 		  })
-		  const list = element.split(";")
-		  var newList = []
-		  list.forEach(item =>{
-			 if(item !== ''){
-			 	newList.push(item.slice(2))
-			 }
-		  })
-		  this.elementList = newList
+		  if(element !== null){
+			  const list = element.split(";")
+			  var newList = []
+			  list.forEach(item =>{
+			  			 if(item !== ''){
+			  			 	newList.push(item.slice(2))
+			  			 }
+			  })
+			  return this.elementList = newList
+		  }
 	  },
 	  
 	  //复制名称
@@ -261,16 +288,66 @@
 		  			})
 	  },
 	  //复制编码
-	  copyHscode(){
+	  copyHscode(hscode){
 		  uni.setClipboardData({
-		  				data: this.hscode
+		  				data: hscode
 		  			})
 	  },
+	  //复制分类
+	  copyCate(cate){
+		  uni.setClipboardData({
+		  				data: cate
+		  			})
+	  },
+	  
 	  //返回首页
 	  goHome(){
 	  		  this.$Router.push('/pages/home/Home')
-	  }
-    },
+	  },
+	  
+	  //处理监管与检疫条件列表
+	  codeList(){
+		  //处理监管条件
+		  var codeLists = []
+		  this.goodList.forEach(item =>{
+		  	  return codeLists = item.regulatory_code_name
+		  })
+		  if(codeLists !== null){
+			  codeLists.forEach(item =>{
+				  let name = item.split(':')
+				  this.codeName.push(name[0])
+				  this.codeDetail.push(name[1])
+			  })
+		  }
+	  },
+	  
+	  ciqList(){
+		  //处理检疫条件
+		  var ciqLists = []
+		  this.goodList.forEach(item =>{
+		  	  return ciqLists = item.ciq_code_name
+		  })
+		  if(ciqLists !== null){
+			  ciqLists.forEach(item =>{
+			  			  let name = item.split(':')
+			  			  this.ciqName.push(name[0])
+			  			  this.ciqDetail.push(name[1])
+			  })
+			}
+	  },
+	  
+	  //税率单位换算
+	  changeNumber(){
+		  this.goodList.forEach(item =>{
+			  //保留0位小数
+			  item.ordinary = (item.ordinary * 100).toFixed(0) + '%'
+			  item.most_discount = (item.most_discount * 100).toFixed(0) + '%'
+			  item.export_rate = (item.export_rate * 100).toFixed(0) + '%'
+			  item.add_tax_rate = (item.add_tax_rate * 100).toFixed(0) + '%'
+			  item.customs_rate = (item.customs_rate * 100).toFixed(0) + '%'
+		  })
+    }
+	},
     mounted() {
       this.getListByHsCode()
     },
